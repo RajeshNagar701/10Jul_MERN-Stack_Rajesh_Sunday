b2a99e670a89872a9894cdf95156ce7822c05bc4
GauravGoswami12967 <gaurav12967@gmail.com>
2024-04-21 11:36:56 +0530

    changes

---
+++ TASKsrc/App.js
@@ -0,0 +1,30 @@
+import CSS from "./CSS/CSS";
+import Class_compo from "./Component/Class_compo";
+import Func_compo from "./Component/Func_compo";
+import JSX from "./JSX/JSX";
+import Module_css from "./Module_css/Module_css";
+import Sass from "./Sass/Sass";
+
+function App(){
+    return(
+        <>
+            {
+                // Component
+                //<Func_compo/>
+                //<Class_compo/>
+
+
+                // jsx
+                //<JSX/>
+
+                // css /  module css / sass
+                //<CSS/>
+                //<Module_css/>
+                <Sass/>
+            }
+            
+        </>
+    )
+}
+
+export default App;
